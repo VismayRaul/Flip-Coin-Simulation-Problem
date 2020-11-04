5989240c29b95d8926bf96a8c19634dd7fd4fac6
VismayRaul <vismayraul@gmail.com>
2020-11-04 23:27:07 +0530

    flip coin multiple times in loop

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash
 
-heads=0;
-tails=1;
+head=0;
+tail=1;
 
-result=$((RANDOM%2))
+declare -A result
 
-if [[ $result -eq $heads ]]
-then
-	echo heads
-else
-	echo tails
-fi
+for ((i=1;i<50;i++))
+do
+	result[$i]=$((RANDOM%2))
+done
+
+head=`echo ${result[@]} | tr ' ' '\n' | sort -n | uniq -c | head -1 | awk '{print $1}'`
+
+tail=`echo ${result[@]} | tr ' ' '\n' | sort -n | uniq -c | tail -1 | awk '{print $1}'`
+
+echo tail_win_times=$tail
+echo head_win_times=$head
